c4ec4344286640dfe5a5a50c89e6aa9a9ce6a301
xxmarce1oo <marcelo2@ufpr.br>
2025-04-10 19:25:49 -0300

    pagina criada

---
+++ soma/src/app/pages/soma/soma.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soma',
+  imports: [],
+  templateUrl: './soma.component.html',
+  styleUrl: './soma.component.css'
+})
+export class SomaComponent {
+
+}
