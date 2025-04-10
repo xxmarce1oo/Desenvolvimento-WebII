1e15dc685bf9cca5c18f86dc99475d02794553b5
xxmarce1oo <marcelo2@ufpr.br>
2025-04-10 20:05:03 -0300

    soma feita

--- soma/src/app/app.component.ts
+++ soma/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {SomaComponent} from './pages/soma';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, SomaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- soma/src/app/pages/soma/soma.component.ts
+++ soma/src/app/pages/soma/soma.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {SomaService} from '../../services';
 
 @Component({
   selector: 'app-soma',
+  standalone: true,
   imports: [],
   templateUrl: './soma.component.html',
   styleUrl: './soma.component.css'
 })
 export class SomaComponent {
+private res: number = 0;
+  constructor(private somaService:SomaService) {}
 
-}
+  somarBotao(numero1:string,numero2:string):void{
+    let n1:number;
+    let n2:number;
+
+    n1 = parseFloat(numero1)
+    n2 = parseFloat(numero2)
+
+    this.res = this.somaService.somar(n1,n2);
+  }
+  get resultado(): string{
+    return this.res.toString()
+  }
+
+  }
